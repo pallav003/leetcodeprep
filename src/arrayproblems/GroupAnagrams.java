9e50f09687d314d943e25eac63b6f35b6e1a193a
pallav003 <pallav.kalal.003@gmail.com>
2025-03-06 21:01:07 +0530

    "neetcode-array-problems"

--- src/arrayproblems/GroupAnagrams.java
+++ src/arrayproblems/GroupAnagrams.java
@@ -1,8 +1,19 @@
 package arrayproblems;
 
 import java.util.*;
-import java.util.stream.StreamSupport;
 
+/**
+ * Group Anagrams
+ * Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+ *
+ * An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+ *
+ * Example 1:
+ *
+ * Input: strs = ["act","pots","tops","cat","stop","hat"]
+ *
+ * Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+ */
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> outputMap = new HashMap<>();
